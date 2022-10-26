5fedf82d371b0d29854ee1acfaee972ed56416fd
ColaCCola <101168138+ColaCCola@users.noreply.github.com>
2022-10-26 20:35:20 +0800

    更新时间

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+        String t1="10:00";
+        String t2="11:00";
+        int h1=Integer.parseInt(t1.substring(0, 2));
+        System.out.println(h1);
+    }
+}
